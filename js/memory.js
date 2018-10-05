40e80e84dcd4beaccb0ae805ef4b59525e1ad38a
Uğur Soğukpınar <sogukpinar.ugur@gmail.com>
2018-10-05 16:49:49 +0300

    take name of the player and show it on the game page.

--- js/memory.js
+++ js/memory.js
@@ -143,4 +143,18 @@ function change(changeit)
 	}
 	
 }
-	
+  
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var username = getParameterByName('name');
+  document.getElementById('nameBox').innerHTML = "Name: " + username;
+}, false);
